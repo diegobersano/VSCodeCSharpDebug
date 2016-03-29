597711578b36df8a090ec6663380373e365c3688
diegobersano <diegobersano33@gmail.com>
2016-03-28 22:26:38 -0300

    Commit inicial

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var test = "Hello World!";
+
+            test = test.ToUpper();
+
+            Console.WriteLine(test);
+            
+            var testClass = new Test(test);
+        }
+    }
+
+    public class Test
+    {
+        public Test(string name)
+        {
+            var a = name;
+        }
+    }
+}
